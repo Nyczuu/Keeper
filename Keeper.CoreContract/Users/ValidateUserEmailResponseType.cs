ef06a6d45e68cd54391543539e3c2116025edc8b
Mateusz Nycz <mateusz.gsm@gmail.com>
2018-12-14 21:40:34 +0100

    First idea of user email validation

---
+++ Keeper.CoreContract/Users/ValidateUserEmailResponseType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Keeper.CoreContract.Users
+{
+    public enum ValidateUserEmailResponseType
+    {
+        IsValid = 0,
+        IsEmpty = 1,
+        WrongFormat = 2,
+        AlreadyExists = 3,
+    }
+}
