b45b919b017ca5a68de970d81508bdd7ba53af0f
MateuszNycz <mateusz.gsm@gmail.com>
2019-01-06 18:59:49 +0100

    LoginUser small cleanup

--- Keeper.Core/Users/LoginUser.cs
+++ Keeper.Core/Users/LoginUser.cs
@@ -1,13 +1,9 @@
-﻿using Keeper.CoreContract;
+﻿using Keeper.Core.Helpers;
 using Keeper.CoreContract.Users;
 using Keeper.Data;
-using Keeper.Data.Migrations;
 using Keeper.Data.Users;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Keeper.Core.Users
 {
@@ -17,33 +13,36 @@ namespace Keeper.Core.Users
 
         public LoginUser(LoginUserRequest request)
         {
-            Response = new LoginUserResponse();
             if (request != null)
             {
-                if (string.IsNullOrWhiteSpace(request.Email)) // todo: regex 
-                    Response.Type = LoginUserResponseType.WrongEmail;
-                else if (string.IsNullOrWhiteSpace(request.Password))
-                    Response.Type = LoginUserResponseType.WrongPassword;
+                if (string.IsNullOrWhiteSpace(request.Email)
+                    || !EmailHelper.IsValidEmail(request.Email))
+                    Response = new LoginUserResponse
+                    { Type = LoginUserResponseType.WrongEmail };
                 else
                     using (var dbContext = new ApplicationDbContext())
                     {
                         var user = dbContext.Users.SingleOrDefault(aUser
-                            => aUser.Email.ToLower()
-                            == request.Email.ToLower());
+                            => aUser.Email.Trim().ToLower()
+                            == request.Email.Trim().ToLower());
                         if (user == null)
-                            Response.Type = LoginUserResponseType.NoUser;
-                        else
-                            if (user.Password != request.Password)
-                            Response.Type = LoginUserResponseType.WrongPassword;
+                            Response = new LoginUserResponse
+                            { Type = LoginUserResponseType.NoUser };
+                        else if (user.Password != request.Password)
+                            Response = new LoginUserResponse
+                            { Type = LoginUserResponseType.WrongPassword };
                         else
                         {
                             var userSessionId = Guid.NewGuid();
                             var userSession = new UserSession();
                             userSession.Set(request, userSessionId, user.Identifier);
-                            dbContext.SaveChanges(); 
+                            dbContext.SaveChanges();
 
-                            Response.Type = LoginUserResponseType.Success;
-                            Response.SessionId = userSessionId;
+                            Response = new LoginUserResponse
+                            {
+                                Type = LoginUserResponseType.Success,
+                                SessionId = userSessionId,
+                            };
                         }
                     }
             }
