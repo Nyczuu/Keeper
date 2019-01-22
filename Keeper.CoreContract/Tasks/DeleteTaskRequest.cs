e33bb2db4aeccaa0b511dbbdc6889f1df92ea540
lmln <lmilenkowicz@gmail.com>
2019-01-22 21:18:00 +0100

    gettask i deletetask

---
+++ Keeper.CoreContract/Tasks/DeleteTaskRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Keeper.CoreContract.Tasks
+{
+    public class DeleteTaskRequest
+    {
+        public int[] Identifiers;
+    }
+}
